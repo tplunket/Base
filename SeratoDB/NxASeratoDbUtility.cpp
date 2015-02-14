58c164248e5a56de82f6de26129f3560c7a50719
Didier Malenfant <didier@nextaudiolabs.com>
2015-02-13 16:52:12 -0800

    Added missing confidentiality notice.

--- SeratoDB/NxASeratoDbUtility.cpp
+++ SeratoDB/NxASeratoDbUtility.cpp
@@ -5,6 +5,14 @@
 //  Created by Didier Malenfant on 2/4/15.
 //  Copyright (c) 2015 Next Audio Labs, LLC. All rights reserved.
 //
+//  This file contains confidential and proprietary information of Serato
+//  Inc. LLP ("Serato"). No use is permitted without express written
+//  permission of Serato. If you are not a party to a Confidentiality/
+//  Non-Disclosure Agreement with Serato, please immediately delete this
+//  file as well as all copies in your possession. For further information,
+//  please refer to the modified MIT license provided with this library,
+//  or email licensing@serato.com.
+//
 
 #include "NxASeratoDbUtility.h"
 

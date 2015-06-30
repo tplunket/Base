207ae463f091bd950bf56f725fb696d284524414
Didier Malenfant <didier@nextaudiolabs.com>
2015-06-30 14:38:25 -0700

    Added unit tests for Blob.

--- Base/Tests/Tests.hpp
+++ Base/Tests/Tests.hpp
@@ -26,7 +26,9 @@
 // -- This forces the linker to link the object files where these tests
 // -- are defined. Otherwise they would get stripped out.
 NXA_USING_TEST_SUITE_NAMED(Base_String_Tests);
+NXA_USING_TEST_SUITE_NAMED(Base_Blob_Tests);
 
 NXA_USE_TEST_SUITES_FOR_MODULE(Base) {
-    Base_String_Tests
+    Base_String_Tests,
+    Base_Blob_Tests
 };

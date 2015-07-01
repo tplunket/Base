f7ae4deb2b1acc5f23b63e19541b67a976f40888
Didier Malenfant <didier@nextaudiolabs.com>
2015-07-01 12:17:24 -0700

    Fixed incorrect type identifier used in tests and added more tests.

--- SeratoDB/Tags/Tests/UInt32TagTests.cpp
+++ SeratoDB/Tags/Tests/UInt32TagTests.cpp
@@ -10,7 +10,6 @@
 //
 
 #include "Tags/UInteger32Tag.hpp"
-#include "Base/Base.hpp"
 #include "Base/Test.hpp"
 
 using namespace testing;
@@ -19,40 +18,65 @@ using namespace NxA::Serato;
 
 NXA_CONTAINS_TEST_SUITE_NAMED(SeratoDB_UInteger32Tag_Tests);
 
-TEST(SeratoDB_UInteger32Tag, TagCreatedFromNormalConstructor_HasCorrectValue)
+TEST(SeratoDB_UInteger32Tag, TagWithIdentifierAndValue_TagCreated_TagHasCorrectValue)
 {
+    // -- Given.
     // -- When.
-    auto tag = UInteger32Tag::tagWithIdentifierAndValue('vrsn', 0xDEADBEEF);
+    auto tag = UInteger32Tag::tagWithIdentifierAndValue('utst', 0xDEADBEEF);
 
     // -- Then.
+    ASSERT_EQ('utst', tag->identifier());
     ASSERT_EQ(0xDEADBEEF, tag->value());
 }
 
-TEST(SeratoDB_UInteger32Tag, TagCreatedFromParsingConstructor_HasCorrectValue)
+TEST(SeratoDB_UInteger32Tag, TagWithMemoryAt_TagCreated_TagHasCorrectValue)
 {
     // -- Given.
-    constexpr unsigned char data[] = { 0x75, 0x61, 0x64, 0x64, 0x00, 0x00, 0x00, 0x04, 0x54, 0xCA, 0x84, 0xAB };
+    constexpr byte data[] = { 0x75, 0x74, 0x73, 0x74, 0x00, 0x00, 0x00, 0x04, 0x54, 0xCA, 0x84, 0xAB };
 
     // -- When.
     auto tag = UInteger32Tag::tagWithMemoryAt(data);
 
     // -- Then.
+    ASSERT_EQ('utst', tag->identifier());
     ASSERT_EQ(0x54CA84AB, tag->value());
 }
 
-TEST(SeratoDB_UInteger32Tag, TagWrittenToData_HasCorrectOutput)
+TEST(SeratoDB_UInteger32Tag, TagWithMemoryAt_TagCreatedFromDataWithIncorrectSize_ThrowsException)
 {
-    // -- When.
-    auto tag = UInteger32Tag::tagWithIdentifierAndValue('vrsn', 0xDEADBEEF);
+    // -- Given.
+    constexpr byte data[] = { 0x75, 0x74, 0x73, 0x74, 0x00, 0x00, 0x00, 0x02, 0x54, 0xCA };
 
     // -- When.
+    // -- Then.
+    ASSERT_THROW(UInteger32Tag::tagWithMemoryAt(data), NxA::Exception);
+}
+
+TEST(SeratoDB_UInteger32Tag, SetValue_TagWithAValue_SetsTheValueCorrectly)
+{
+    // -- Given.
+    auto tag = UInteger32Tag::tagWithIdentifierAndValue('utst', 0xDEADBEEF);
+
+    // -- When.
+    tag->setValue(0x23232323);
+
+    // -- Then.
+    ASSERT_EQ('utst', tag->identifier());
+    ASSERT_EQ(0x23232323, tag->value());
+}
+
+TEST(SeratoDB_UInteger32Tag, AddTo_TagWrittenToBlob_WriteCorrectData)
+{
+    // -- Given.
+    auto tag = UInteger32Tag::tagWithIdentifierAndValue('utst', 0xDEADBEEF);
     auto destination = Blob::blob();
+
+    // -- When.
     tag->addTo(destination);
 
     // -- Then.
-    ASSERT_EQ(destination->size(), 12);
-    constexpr uinteger8 expectedData[] = { 0x76, 0x72, 0x73, 0x6E, 0x00, 0x00, 0x00, 0x04, 0xDE, 0xAD, 0xBE, 0xEF };
-    for (uint32_t index = 0; index < sizeof(expectedData); ++index) {
-        ASSERT_EQ(expectedData[index], (*destination)[index]);
-    }
+    auto data = destination->data();
+    constexpr byte expectedData[] = { 0x75, 0x74, 0x73, 0x74, 0x00, 0x00, 0x00, 0x04, 0xDE, 0xAD, 0xBE, 0xEF };
+    ASSERT_EQ(sizeof(expectedData), destination->size());
+    ASSERT_EQ(0, ::memcmp(expectedData, data, sizeof(expectedData)));
 }

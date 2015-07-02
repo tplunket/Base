675610604a55aee283f9ea845eebcf9a8a8a6d6e
Didier Malenfant <didier@nextaudiolabs.com>
2015-07-02 11:08:06 -0700

    Throws an exception when trying to make an object tag with an empty array.

--- SeratoDB/Tags/ObjectTag.cpp
+++ SeratoDB/Tags/ObjectTag.cpp
@@ -34,6 +34,7 @@ ObjectTag::Pointer ObjectTag::tagWithMemoryAt(const byte* tagAddress)
 
 ObjectTag::Pointer ObjectTag::tagWithIdentifierAndValue(uinteger32 identifier, const Tag::Array& content)
 {
+    NXA_ASSERT_NOT_EQ(content.length(), 0);
     NXA_ASSERT_EQ((identifier & 0xFF000000) >> 24, 'o');
 
     auto newTag = ObjectTag::makeShared();

1100325575f719dd235a6c4de920286a2274c06a
Didier Malenfant <didier@nextaudiolabs.com>
2015-07-28 16:00:43 -0700

    Missing void for method with no arguments.

--- SeratoDB/TrackFiles/Internal/TrackFile.hpp
+++ SeratoDB/TrackFiles/Internal/TrackFile.hpp
@@ -58,7 +58,7 @@ namespace NxA { namespace Serato { namespace Internal {
         void readMarkersV2FromBase64String(const byte* markerV2Data, count totalSize);
         void readGridMarkersFrom(const byte* gridMarkerData);
         void addGridMarker(Serato::GridMarker& gridMarker);
-        String::Pointer base64StringFromMarkersV2();
+        String::Pointer base64StringFromMarkersV2(void);
         Blob::Pointer gridMarkerDataFromGridMarkers(void);
         virtual void writeMarkers(void) = 0;
     };

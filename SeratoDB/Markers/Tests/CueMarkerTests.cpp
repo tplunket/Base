e1d1039f70911ea219b4299f2bb3931b4f8aabd3
Didier Malenfant <didier@nextaudiolabs.com>
2015-09-16 15:52:16 -0700

    Fixed and added unit tests for marker colors.

--- SeratoDB/Markers/Tests/CueMarkerTests.cpp
+++ SeratoDB/Markers/Tests/CueMarkerTests.cpp
@@ -23,7 +23,7 @@ TEST(SeratoDB_CueMarker, markerWithMemoryAt_ASeratoMarkerTag_ReturnsACorrectMark
     // -- Given.
     constexpr byte data[] = {
         0x43, 0x55, 0x45, 0x00, 0x00, 0x00, 0x00, 0x11, 0x00, 0x01, 0x00,
-        0x02, 0xDD, 0x66, 0x00, 0xCC, 0x88, 0x00, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
+        0x02, 0xDD, 0x66, 0x00, 0x45, 0xdc, 0x02, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
     };
 
     // -- When.
@@ -33,6 +33,9 @@ TEST(SeratoDB_CueMarker, markerWithMemoryAt_ASeratoMarkerTag_ReturnsACorrectMark
     ASSERT_EQ(187750, test->positionInMilliseconds());
     ASSERT_EQ(1, test->index());
     ASSERT_STREQ("TEST", test->label().toUTF8());
+    ASSERT_EQ(0x45, test->colorRedComponent());
+    ASSERT_EQ(0xdc, test->colorGreenComponent());
+    ASSERT_EQ(0x02, test->colorBlueComponent());
 }
 
 TEST(SeratoDB_CueMarker, markerWithMemoryAt_ASeratoMarkerTagWithAnInvalidTagName_ThrowsAnException)
@@ -48,22 +51,25 @@ TEST(SeratoDB_CueMarker, markerWithMemoryAt_ASeratoMarkerTagWithAnInvalidTagName
     ASSERT_THROW(CueMarker::markerWithMemoryAt(data), NxA::Exception);
 }
 
-TEST(SeratoDB_CueMarker, markerWithLabelPositionAndIndex_AMarkerWithInitialValues_ReturnsACorrectMarker)
+TEST(SeratoDB_CueMarker, markerWithLabelPositionIndexAndColor_AMarkerWithInitialValues_ReturnsACorrectMarker)
 {
     // -- Given.
     // -- When.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
 
     // -- Then.
     ASSERT_EQ(34985, test->positionInMilliseconds());
     ASSERT_EQ(43, test->index());
     ASSERT_STREQ("TEST", test->label().toUTF8());
+    ASSERT_EQ(0x45, test->colorRedComponent());
+    ASSERT_EQ(0xdc, test->colorGreenComponent());
+    ASSERT_EQ(0x02, test->colorBlueComponent());
 }
 
 TEST(SeratoDB_CueMarker, markerWith_AMarkerAsSource_ReturnsACorrectMarker)
 {
     // -- Given.
-    auto initial = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
+    auto initial = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
 
     // -- When.
     auto test = CueMarker::markerWith(initial);
@@ -72,13 +78,16 @@ TEST(SeratoDB_CueMarker, markerWith_AMarkerAsSource_ReturnsACorrectMarker)
     ASSERT_EQ(34985, test->positionInMilliseconds());
     ASSERT_EQ(43, test->index());
     ASSERT_STREQ("TEST", test->label().toUTF8());
+    ASSERT_EQ(0x45, test->colorRedComponent());
+    ASSERT_EQ(0xdc, test->colorGreenComponent());
+    ASSERT_EQ(0x02, test->colorBlueComponent());
 }
 
 TEST(SeratoDB_CueMarker, OperatorEqual_TwoEqualMarkers_ReturnsTrue)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
 
     // -- When.
     // -- Then.
@@ -88,8 +97,8 @@ TEST(SeratoDB_CueMarker, OperatorEqual_TwoEqualMarkers_ReturnsTrue)
 TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentIndex_ReturnsFalse)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 3);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 3, 0x45, 0xdc, 0x02);
 
     // -- When.
     // -- Then.
@@ -99,8 +108,8 @@ TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentIndex_ReturnsFa
 TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentLabel_ReturnsFalse)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST2"), 34985, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST2"), 34985, 43, 0x45, 0xdc, 0x02);
 
     // -- When.
     // -- Then.
@@ -110,8 +119,41 @@ TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentLabel_ReturnsFa
 TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentPos_ReturnsFalse)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34984, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34984, 43, 0x45, 0xdc, 0x02);
+
+    // -- When.
+    // -- Then.
+    ASSERT_FALSE(*test == *other);
+}
+
+TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentRed_ReturnsFalse)
+{
+    // -- Given.
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x44, 0xdc, 0x02);
+
+    // -- When.
+    // -- Then.
+    ASSERT_FALSE(*test == *other);
+}
+
+TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentGreen_ReturnsFalse)
+{
+    // -- Given.
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0x00, 0x02);
+
+    // -- When.
+    // -- Then.
+    ASSERT_FALSE(*test == *other);
+}
+
+TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentBlue_ReturnsFalse)
+{
+    // -- Given.
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x22);
 
     // -- When.
     // -- Then.
@@ -121,8 +163,8 @@ TEST(SeratoDB_CueMarker, OperatorEqual_TwoUnequalMarkersDifferentPos_ReturnsFals
 TEST(SeratoDB_CueMarker, OperatorUnequal_TwoEqualMarkers_ReturnsFalse)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
 
     // -- When.
     // -- Then.
@@ -132,8 +174,8 @@ TEST(SeratoDB_CueMarker, OperatorUnequal_TwoEqualMarkers_ReturnsFalse)
 TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentIndex_ReturnsTrue)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 3);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 3, 0x45, 0xdc, 0x02);
 
     // -- When.
     // -- Then.
@@ -143,8 +185,8 @@ TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentIndex_Returns
 TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentLabel_ReturnsTrue)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST2"), 34985, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST2"), 34985, 43, 0x45, 0xdc, 0x02);
 
     // -- When.
     // -- Then.
@@ -154,8 +196,41 @@ TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentLabel_Returns
 TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentPosition_ReturnsTrue)
 {
     // -- Given.
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34985, 43);
-    auto other = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 34986, 43);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34986, 43, 0x45, 0xdc, 0x02);
+
+    // -- When.
+    // -- Then.
+    ASSERT_TRUE(*test != *other);
+}
+
+TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentRed_ReturnsTrue)
+{
+    // -- Given.
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x44, 0xdc, 0x02);
+
+    // -- When.
+    // -- Then.
+    ASSERT_TRUE(*test != *other);
+}
+
+TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentGreen_ReturnsTrue)
+{
+    // -- Given.
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0x0c, 0x02);
+
+    // -- When.
+    // -- Then.
+    ASSERT_TRUE(*test != *other);
+}
+
+TEST(SeratoDB_CueMarker, OperatorUnequal_TwoUnequalMarkersDifferentBlue_ReturnsTrue)
+{
+    // -- Given.
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x02);
+    auto other = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 34985, 43, 0x45, 0xdc, 0x00);
 
     // -- When.
     // -- Then.
@@ -166,7 +241,7 @@ TEST(SeratoDB_CueMarker, addId3TagTo_AMarkerWithData_WritesTheCorrectData)
 {
     // -- Given.
     auto destination = Blob::blob();
-    auto test = CueMarker::markerWithLabelPositionAndIndex(String::stringWith("TEST"), 187750, 1);
+    auto test = CueMarker::markerWithLabelPositionIndexAndColor(String::stringWith("TEST"), 187750, 1, 0x45, 0xdc, 0x02);
 
     // -- When.
     test->addId3TagTo(destination);
@@ -175,7 +250,7 @@ TEST(SeratoDB_CueMarker, addId3TagTo_AMarkerWithData_WritesTheCorrectData)
     auto data = destination->data();
     constexpr byte expectedData[] = {
         0x43, 0x55, 0x45, 0x00, 0x00, 0x00, 0x00, 0x11, 0x00, 0x01, 0x00,
-        0x02, 0xDD, 0x66, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
+        0x02, 0xDD, 0x66, 0x00, 0x45, 0xdc, 0x02, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
     };
     ASSERT_EQ(sizeof(expectedData), destination->size());
     ASSERT_EQ(0, ::memcmp(expectedData, data, sizeof(expectedData)));
--- SeratoDB/Markers/Tests/LoopMarkerTests.cpp
+++ SeratoDB/Markers/Tests/LoopMarkerTests.cpp
@@ -23,7 +23,7 @@ TEST(SeratoDB_LoopMarker, markerWithMemoryAt_ASeratoMarkerTag_ReturnsACorrectMar
     // -- Given.
     constexpr byte data[] = {
         0x4C, 0x4F, 0x4F, 0x50, 0x00, 0x00, 0x00, 0x00, 0x19, 0x00, 0x00, 0x00, 0x02, 0x22, 0x47,
-        0x00, 0x02, 0x31, 0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x27, 0xAA, 0xE1, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
+        0x00, 0x02, 0x31, 0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x22, 0xff, 0xcc, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
     };
 
     // -- When.
@@ -34,6 +34,9 @@ TEST(SeratoDB_LoopMarker, markerWithMemoryAt_ASeratoMarkerTag_ReturnsACorrectMar
     ASSERT_EQ(143718, test->endPositionInMilliseconds());
     ASSERT_EQ(0, test->index());
     ASSERT_STREQ("TEST", test->label().toUTF8());
+    ASSERT_EQ(0x22, test->colorRedComponent());
+    ASSERT_EQ(0xff, test->colorGreenComponent());
+    ASSERT_EQ(0xcc, test->colorBlueComponent());
 }
 
 TEST(SeratoDB_LoopMarker, markerWithMemoryAt_ASeratoMarkerTagWithAnInvalidTagName_ThrowsAnException)
@@ -49,31 +52,34 @@ TEST(SeratoDB_LoopMarker, markerWithMemoryAt_ASeratoMarkerTagWithAnInvalidTagNam
     ASSERT_THROW(LoopMarker::markerWithMemoryAt(data), NxA::AssertionFailed);
 }
 
-TEST(SeratoDB_LoopMarker, markerWithLabelStartEndPositionsAndIndex_AMarkerWithInitialValues_ReturnsACorrectMarker)
+TEST(SeratoDB_LoopMarker, markerWithLabelStartEndPositionsIndexAndColor_AMarkerWithInitialValues_ReturnsACorrectMarker)
 {
     // -- Given.
     // -- When.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- Then.
     ASSERT_EQ(139847, test->startPositionInMilliseconds());
     ASSERT_EQ(143718, test->endPositionInMilliseconds());
     ASSERT_EQ(0, test->index());
+    ASSERT_EQ(0x22, test->colorRedComponent());
+    ASSERT_EQ(0xff, test->colorGreenComponent());
+    ASSERT_EQ(0xcc, test->colorBlueComponent());
     ASSERT_STREQ("TEST", test->label().toUTF8());
 }
 
-TEST(SeratoDB_LoopMarker, markerWithLabelStartEndPositionsAndIndex_AMarkerWithInvalidStartEndValues_ThrowAnException)
+TEST(SeratoDB_LoopMarker, markerWithLabelStartEndPositionsIndexAndColor_AMarkerWithInvalidStartEndValues_ThrowAnException)
 {
     // -- Given.
     // -- When.
     // -- Then.
-    ASSERT_THROW(LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 159847, 143718, 0), NxA::Serato::LoopMarkerError);
+    ASSERT_THROW(LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 159847, 143718, 0, 0x22, 0xff, 0xcc), NxA::Serato::LoopMarkerError);
 }
 
 TEST(SeratoDB_LoopMarker, markerWith_AMarkerAsSource_ReturnsACorrectMarker)
 {
     // -- Given.
-    auto initial = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
+    auto initial = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     auto test = LoopMarker::markerWith(initial);
@@ -82,14 +88,17 @@ TEST(SeratoDB_LoopMarker, markerWith_AMarkerAsSource_ReturnsACorrectMarker)
     ASSERT_EQ(139847, test->startPositionInMilliseconds());
     ASSERT_EQ(143718, test->endPositionInMilliseconds());
     ASSERT_EQ(0, test->index());
+    ASSERT_EQ(0x22, test->colorRedComponent());
+    ASSERT_EQ(0xff, test->colorGreenComponent());
+    ASSERT_EQ(0xcc, test->colorBlueComponent());
     ASSERT_STREQ("TEST", test->label().toUTF8());
 }
 
 TEST(SeratoDB_LoopMarker, OperatorEqual_TwoEqualMarkers_ReturnsTrue)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -99,8 +108,8 @@ TEST(SeratoDB_LoopMarker, OperatorEqual_TwoEqualMarkers_ReturnsTrue)
 TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentIndex_ReturnsFalse)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 2);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 2, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -110,8 +119,8 @@ TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentIndex_ReturnsF
 TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentLabel_ReturnsFalse)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST2"), 139847, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST2"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -121,8 +130,8 @@ TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentLabel_ReturnsF
 TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentStart_ReturnsFalse)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139837, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139837, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -132,8 +141,41 @@ TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentStart_ReturnsF
 TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentEnd_ReturnsFalse)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143720, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143720, 0, 0x22, 0xff, 0xcc);
+
+    // -- When.
+    // -- Then.
+    ASSERT_FALSE(*test == *other);
+}
+
+TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentRed_ReturnsFalse)
+{
+    // -- Given.
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x21, 0xff, 0xcc);
+
+    // -- When.
+    // -- Then.
+    ASSERT_FALSE(*test == *other);
+}
+
+TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentGreen_ReturnsFalse)
+{
+    // -- Given.
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xef, 0xcc);
+
+    // -- When.
+    // -- Then.
+    ASSERT_FALSE(*test == *other);
+}
+
+TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentBlue_ReturnsFalse)
+{
+    // -- Given.
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0x00);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -143,8 +185,8 @@ TEST(SeratoDB_LoopMarker, OperatorEqual_TwoUnequalMarkersDifferentEnd_ReturnsFal
 TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoEqualMarkers_ReturnsFalse)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -154,8 +196,8 @@ TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoEqualMarkers_ReturnsFalse)
 TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentIndex_ReturnsTrue)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 2);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 2, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -165,8 +207,8 @@ TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentIndex_Return
 TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentLabel_ReturnsTrue)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST2"), 139847, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST2"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -176,8 +218,8 @@ TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentLabel_Return
 TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentStart_ReturnsTrue)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139843, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0x00);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139843, 143718, 0, 0x22, 0xff, 0x00);
 
     // -- When.
     // -- Then.
@@ -187,8 +229,41 @@ TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentStart_Return
 TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentEnd_ReturnsTrue)
 {
     // -- Given.
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
-    auto other = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143711, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0x00);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143711, 0, 0x22, 0xff, 0x00);
+
+    // -- When.
+    // -- Then.
+    ASSERT_TRUE(*test != *other);
+}
+
+TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentRed_ReturnsFalse)
+{
+    // -- Given.
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x21, 0xff, 0xcc);
+
+    // -- When.
+    // -- Then.
+    ASSERT_TRUE(*test != *other);
+}
+
+TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentGreen_ReturnsFalse)
+{
+    // -- Given.
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xef, 0xcc);
+
+    // -- When.
+    // -- Then.
+    ASSERT_TRUE(*test != *other);
+}
+
+TEST(SeratoDB_LoopMarker, OperatorUnequal_TwoUnequalMarkersDifferentBlue_ReturnsFalse)
+{
+    // -- Given.
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0x00);
+    auto other = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     // -- Then.
@@ -199,7 +274,7 @@ TEST(SeratoDB_LoopMarker, addId3TagTo_AMarkerWithData_WritesTheCorrectData)
 {
     // -- Given.
     auto destination = Blob::blob();
-    auto test = LoopMarker::markerWithLabelStartEndPositionsAndIndex(String::stringWith("TEST"), 139847, 143718, 0);
+    auto test = LoopMarker::markerWithLabelStartEndPositionsIndexAndColor(String::stringWith("TEST"), 139847, 143718, 0, 0x22, 0xff, 0xcc);
 
     // -- When.
     test->addId3TagTo(destination);
@@ -208,7 +283,7 @@ TEST(SeratoDB_LoopMarker, addId3TagTo_AMarkerWithData_WritesTheCorrectData)
     auto data = destination->data();
     constexpr byte expectedData[] = {
         0x4C, 0x4F, 0x4F, 0x50, 0x00, 0x00, 0x00, 0x00, 0x19, 0x00, 0x00, 0x00, 0x02, 0x22, 0x47,
-        0x00, 0x02, 0x31, 0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
+        0x00, 0x02, 0x31, 0x66, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x22, 0xff, 0xcc, 0x00, 0x00, 0x54, 0x45, 0x53, 0x54, 0x00
     };
     ASSERT_EQ(sizeof(expectedData), destination->size());
     ASSERT_EQ(0, ::memcmp(expectedData, data, sizeof(expectedData)));

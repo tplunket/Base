7ec43834b09b8b98c465d6b06687f1948b101ce3
Didier Malenfant <didier@nextaudiolabs.com>
2015-09-17 11:06:29 -0700

    If we don't delete markers V1 for MP4 files, it seems like this circumvents a bug in Serato.
    When V1 is not present, it seems like Serato doesn't read V2 correctly with MP4 files.

--- SeratoDB/TrackFiles/Internal/MP4TrackFile.cpp
+++ SeratoDB/TrackFiles/Internal/MP4TrackFile.cpp
@@ -35,7 +35,6 @@ namespace NxA { namespace Serato { namespace Internal {
     } MP4MarkersHeaderStruct;
 
     #pragma mark Constants
-    constexpr const character* mp4MarkersItemName = "----:com.serato.dj:markers";
     constexpr const character* mp4MarkersV2ItemName = "----:com.serato.dj:markersv2";
     constexpr const character* mp4BeatgridItemName = "----:com.serato.dj:beatgrid";
 
@@ -196,8 +195,6 @@ void MP4TrackFile::replaceGridMarkersItem(void)
 
 void MP4TrackFile::writeMarkers(void)
 {
-    this->mp4Tag->removeItem(mp4MarkersItemName);
-
     this->replaceMarkersV2Item();
     this->replaceGridMarkersItem();
 }

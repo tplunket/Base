1fcbe1bbda3f9faa6546ef73a8b25f3b6c874661
Didier Malenfant <didier@nextaudiolabs.com>
2015-11-04 20:21:18 -0800

    TrackFile exceptions are caught now.

--- SeratoDB/Internal/Track.cpp
+++ SeratoDB/Internal/Track.cpp
@@ -154,72 +154,89 @@ String::Pointer Track::trackFilePath(void) const
 
 void Track::readMarkers(void)
 {
-    auto trackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
+    try {
+        auto trackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
 
-    auto& allCueMarkers = trackFile->cueMarkers();
-    for (auto& marker : allCueMarkers) {
+        auto& allCueMarkers = trackFile->cueMarkers();
+        for (auto& marker : allCueMarkers) {
 #if NXA_PRINT_DEBUG_INFO
-        printf("Found cue marker at %f and index %d.\n", (float)marker->positionInMilliseconds()/1000, marker->index());
+            printf("Found cue marker at %f and index %d.\n", (float)marker->positionInMilliseconds()/1000, marker->index());
 #endif
-        this->cueMarkers->append(Serato::CueMarker::markerWith(marker));
-    }
+            this->cueMarkers->append(Serato::CueMarker::markerWith(marker));
+        }
 
-    auto& allLoopMarkers = trackFile->loopMarkers();
-    for (auto& marker : allLoopMarkers) {
+        auto& allLoopMarkers = trackFile->loopMarkers();
+        for (auto& marker : allLoopMarkers) {
 #if NXA_PRINT_DEBUG_INFO
-        printf("Found loop marker at %f and index %d.\n", (float)marker->startPositionInMilliseconds()/1000, marker->index());
+            printf("Found loop marker at %f and index %d.\n", (float)marker->startPositionInMilliseconds()/1000, marker->index());
 #endif
-        this->loopMarkers->append(Serato::LoopMarker::markerWith(marker));
-    }
+            this->loopMarkers->append(Serato::LoopMarker::markerWith(marker));
+        }
 
-    auto& allGridMarkers = trackFile->gridMarkers();
-    for (auto& marker : allGridMarkers) {
+        auto& allGridMarkers = trackFile->gridMarkers();
+        for (auto& marker : allGridMarkers) {
 #if NXA_PRINT_DEBUG_INFO
-        printf("Found grid marker at %f and bpm %f.\n", marker->positionInSeconds(), marker->beatsPerMinute());
+            printf("Found grid marker at %f and bpm %f.\n", marker->positionInSeconds(), marker->beatsPerMinute());
 #endif
-        this->gridMarkers->append(Serato::GridMarker::markerWith(marker));
+            this->gridMarkers->append(Serato::GridMarker::markerWith(marker));
+        }
+
+        this->lastMarkersModificationDate = File::modificationDateInSecondsSince1970ForFile(this->trackFilePath());
+        
+        this->trackFilemarkersWereRead = true;
     }
-
-    this->lastMarkersModificationDate = File::modificationDateInSecondsSince1970ForFile(this->trackFilePath());
-
-    this->trackFilemarkersWereRead = true;
+    catch (TrackFileError& exception) {
+        // TODO: This should be logged.
+        return;
+    }
 }
 
 void Track::saveToTrackFile(void) const
 {
-    auto trackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
+    try {
+        auto trackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
 
-    auto newCueMarkers = Serato::CueMarker::Array::array();
-    for (auto& marker : *(this->cueMarkers)) {
-        auto markerCopy = Serato::CueMarker::markerWith(marker);
-        newCueMarkers->append(markerCopy);
+        auto newCueMarkers = Serato::CueMarker::Array::array();
+        for (auto& marker : *(this->cueMarkers)) {
+            auto markerCopy = Serato::CueMarker::markerWith(marker);
+            newCueMarkers->append(markerCopy);
+        }
+        trackFile->setCueMarkers(newCueMarkers);
+
+        auto newLoopMarkers = Serato::LoopMarker::Array::array();
+        for (auto& marker : *(this->loopMarkers)) {
+            auto markerCopy = Serato::LoopMarker::markerWith(marker);
+            newLoopMarkers->append(markerCopy);
+        }
+        trackFile->setLoopMarkers(newLoopMarkers);
+
+        auto newGridMarkers = Serato::GridMarker::Array::array();
+        for (auto& marker : *(this->gridMarkers)) {
+            auto markerCopy = Serato::GridMarker::markerWith(marker);
+            newGridMarkers->append(markerCopy);
+        }
+        trackFile->setGridMarkers(newGridMarkers);
+
+        trackFile->saveChangesIfAny();
     }
-    trackFile->setCueMarkers(newCueMarkers);
-
-    auto newLoopMarkers = Serato::LoopMarker::Array::array();
-    for (auto& marker : *(this->loopMarkers)) {
-        auto markerCopy = Serato::LoopMarker::markerWith(marker);
-        newLoopMarkers->append(markerCopy);
+    catch (TrackFileError& exception) {
+        // TODO: This should be logged.
+        return;
     }
-    trackFile->setLoopMarkers(newLoopMarkers);
-
-    auto newGridMarkers = Serato::GridMarker::Array::array();
-    for (auto& marker : *(this->gridMarkers)) {
-        auto markerCopy = Serato::GridMarker::markerWith(marker);
-        newGridMarkers->append(markerCopy);
-    }
-    trackFile->setGridMarkers(newGridMarkers);
-
-    trackFile->saveChangesIfAny();
 
 #if NXA_PRINT_DEBUG_INFO
     {
-        printf("After saving to track file for '%s' ----------\n", this->trackFilePath()->toUTF8());
-        auto debugtrackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
-        printf("    Found %ld cue markers, %ld grid markers and %ld loop markers.\n",
-               debugtrackFile->cueMarkers().length(),
-               debugtrackFile->gridMarkers().length(),
-               debugtrackFile->loopMarkers().length());
+        try {
+            printf("After saving to track file for '%s' ----------\n", this->trackFilePath()->toUTF8());
+            auto debugtrackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
+            printf("    Found %ld cue markers, %ld grid markers and %ld loop markers.\n",
+                   debugtrackFile->cueMarkers().length(),
+                   debugtrackFile->gridMarkers().length(),
+                   debugtrackFile->loopMarkers().length());
+        } catch (TrackFileError& exception) {
+            printf("Error\n");
+            return;
+        }
     }
 #endif
 }

e335aa14ed5d00682554d86d0b5baf4b9d13f3fc
Didier Malenfant <didier@nextaudiolabs.com>
2015-11-10 14:00:10 -0800

    Removed left over marker code.

--- SeratoDB/Internal/Track.cpp
+++ SeratoDB/Internal/Track.cpp
@@ -42,12 +42,7 @@ static String::Pointer emptyString = String::string();
 Track::Track(Serato::ObjectTag& tag, const String& rootFolderPath) :
              trackTag(tag.pointer()),
              rootFolder(rootFolderPath.pointer()),
-             needsToUpdateDatabaseFile(false),
-             trackFilemarkersWereRead(false),
-             cueMarkers(Serato::CueMarker::ArrayOfConst::array()),
-             loopMarkers(Serato::LoopMarker::ArrayOfConst::array()),
-             gridMarkers(Serato::GridMarker::ArrayOfConst::array()),
-             lastMarkersModificationDate(0)
+             needsToUpdateDatabaseFile(false)
 {
 }
 
@@ -150,90 +145,4 @@ String::Pointer Track::trackFilePath(void) const
 {
     auto& pathFromRootFolder = this->pathForSubTagForIdentifier(trackFilePathTagIdentifier);
     return File::joinPaths(this->rootFolder, pathFromRootFolder);
-}
-
-void Track::readMarkers(void)
-{
-    auto trackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
-
-    auto& allCueMarkers = trackFile->cueMarkers();
-    for (auto& marker : allCueMarkers) {
-#if NXA_PRINT_DEBUG_INFO
-        printf("Found cue marker at %f and index %d.\n", (float)marker->positionInMilliseconds()/1000, marker->index());
-#endif
-        this->cueMarkers->append(Serato::CueMarker::markerWith(marker));
-    }
-
-    auto& allLoopMarkers = trackFile->loopMarkers();
-    for (auto& marker : allLoopMarkers) {
-#if NXA_PRINT_DEBUG_INFO
-        printf("Found loop marker at %f and index %d.\n", (float)marker->startPositionInMilliseconds()/1000, marker->index());
-#endif
-        this->loopMarkers->append(Serato::LoopMarker::markerWith(marker));
-    }
-
-    auto& allGridMarkers = trackFile->gridMarkers();
-    for (auto& marker : allGridMarkers) {
-#if NXA_PRINT_DEBUG_INFO
-        printf("Found grid marker at %f and bpm %f.\n", marker->positionInSeconds(), marker->beatsPerMinute());
-#endif
-        this->gridMarkers->append(Serato::GridMarker::markerWith(marker));
-    }
-
-    this->lastMarkersModificationDate = File::modificationDateInSecondsSince1970ForFile(this->trackFilePath());
-
-    this->trackFilemarkersWereRead = true;
-}
-
-void Track::saveToTrackFile(String::Array& simpleWarningLog) const
-{
-    try {
-        auto trackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
-
-        auto newCueMarkers = Serato::CueMarker::Array::array();
-        for (auto& marker : *(this->cueMarkers)) {
-            auto markerCopy = Serato::CueMarker::markerWith(marker);
-            newCueMarkers->append(markerCopy);
-        }
-        trackFile->setCueMarkers(newCueMarkers);
-
-        auto newLoopMarkers = Serato::LoopMarker::Array::array();
-        for (auto& marker : *(this->loopMarkers)) {
-            auto markerCopy = Serato::LoopMarker::markerWith(marker);
-            newLoopMarkers->append(markerCopy);
-        }
-        trackFile->setLoopMarkers(newLoopMarkers);
-
-        auto newGridMarkers = Serato::GridMarker::Array::array();
-        for (auto& marker : *(this->gridMarkers)) {
-            auto markerCopy = Serato::GridMarker::markerWith(marker);
-            newGridMarkers->append(markerCopy);
-        }
-        trackFile->setGridMarkers(newGridMarkers);
-
-        trackFile->saveChangesIfAny();
-    }
-    catch (TrackFileError& exception) {
-        auto warningString = String::stringWithFormat("Couldn't save changes to track file '%s' because of '%s'.",
-                                                      this->trackFilePath()->toUTF8(),
-                                                      exception.what());
-        simpleWarningLog.append(warningString);
-        return;
-    }
-
-#if NXA_PRINT_DEBUG_INFO
-    {
-        try {
-            printf("After saving to track file for '%s' ----------\n", this->trackFilePath()->toUTF8());
-            auto debugtrackFile = TrackFileFactory::trackFileForPath(this->trackFilePath());
-            printf("    Found %ld cue markers, %ld grid markers and %ld loop markers.\n",
-                   debugtrackFile->cueMarkers().length(),
-                   debugtrackFile->gridMarkers().length(),
-                   debugtrackFile->loopMarkers().length());
-        } catch (TrackFileError& exception) {
-            printf("Error\n");
-            return;
-        }
-    }
-#endif
 }

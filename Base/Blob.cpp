941498fae46657c77bb51e674db9e7581a45d687
Didier Malenfant <didier@nextaudiolabs.com>
2015-12-03 13:44:31 -0800

    Added a method to reset Blobs.

--- Base/Blob.cpp
+++ Base/Blob.cpp
@@ -506,3 +506,8 @@ void Blob::append(const character other)
 {
     this->insert(this->end(), 1, other);
 }
+
+void Blob::removeAll(void)
+{
+    this->std::vector<byte>::clear();
+}
--- Base/Blob.hpp
+++ Base/Blob.hpp
@@ -73,5 +73,7 @@ namespace NxA {
         void appendWithStringTermination(const character* other);
         void appendWithoutStringTermination(const character* other);
         void append(const character other);
+
+        void removeAll(void);
     };
 }

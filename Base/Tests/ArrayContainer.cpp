f331aa2e368abdb791ab77e38866d9e9409ad98e
Didier Malenfant <didier@nextaudiolabs.com>
2016-05-18 22:20:34 -0700

    Fixed include.

--- Base/Tests/ArrayContainer.cpp
+++ Base/Tests/ArrayContainer.cpp
@@ -19,7 +19,7 @@
 //  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#include "Base/String.hpp"
+#include "Base/ArrayContainer.hpp"
 #include "Base/Test.hpp"
 
 using namespace testing;

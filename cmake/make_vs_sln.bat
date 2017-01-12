d671b748e7e14fbe7f97c52fe33927696568c2ab
Tom Plunket <tom@mightysprite.com>
2017-01-12 12:51:01 -0800

    Adds scripts to create project/solution files for Visual Studio and XCode

---
+++ cmake/make_vs_sln.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+cd /d %~dp0
+
+pushd ..
+set SRC_DIR="%CD%"
+popd
+
+call :Create win32 "Visual Studio 14 2015"
+call :Create win64 "Visual Studio 14 2015 Win64"
+goto :eof
+
+:Create
+    set OUTDIR="%~1"
+    set TARGET="%~2"
+    if not exist %OUTDIR% mkdir %OUTDIR%
+    pushd %OUTDIR%
+    cmake -G %TARGET% %SRC_DIR%
+    popd
+    goto :eof
